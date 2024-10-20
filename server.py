0df8ca9e2735211f39a0d58555c858e2e1aa90e8
patricktbp <135556230+n1d3v@users.noreply.github.com>
2024-10-20 14:35:27 +0100

    Clean up alot of the server, add new command types in update.upd.

--- server.py
+++ server.py
@@ -22,33 +22,6 @@ def get_version():
         app.logger.error("Version file not found.")
         return Response("Version file not found.", status=404)
 
-@app.route('/getchangelog', methods=['GET'])
-def get_changelog():
-    changelog_path = 'changelog.txt'
-
-    app.logger.debug(f"Checking if {changelog_path} exists.")
-    if not os.path.exists(changelog_path):
-        app.logger.error("Changelog file not found.")
-        return Response("Changelog file not found.", status=404)
-    
-    app.logger.debug(f"Reading {changelog_path}.")
-    with open(changelog_path, 'r') as file:
-        changelog_content = file.read()
-
-    app.logger.debug("Reading version from version.txt.")
-    version = read_version()
-    if version is None:
-        app.logger.error("Version information not found in version.txt.")
-        return Response("Version information not found in version.txt.", status=400)
-
-    response_data = {
-        'changelog': changelog_content,
-        'version': version
-    }
-    
-    app.logger.debug("Returning response data.")
-    return jsonify(response_data)
-
 @app.route('/updates/<path:filename>', methods=['GET'])
 def download_file(filename):
     updates_dir = 'updates'
